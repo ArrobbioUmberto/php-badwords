a630f74226579da6bb77cce88067f186b42cf29d
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-03-17 16:43:23 +0100

    FEAT add length of text and text_censored

--- censored.php
+++ censored.php
@@ -7,6 +7,8 @@ $text_censored = str_replace($word, '***', $text)
 
 
 <p>TESTO : <?php echo $text ?> </p>
+<span>Lunghezza testo: <?php echo strlen($text) ?></span>
 <span> PAROLA CENSURATA: <?php echo $word ?> </span>
 
 <p>TESTO CENSURATO: <?php echo $text_censored ?></p>
+<span>Lunghezza testo: <?php echo strlen($text_censored) ?></span>
