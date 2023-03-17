7ba288dc9883dbd7d01369f4491298d14803083c
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-03-17 16:38:59 +0100

    DELETE example h1

--- index.php
+++ index.php
@@ -1,10 +1,3 @@
-<?php
-
-$name = $_GET['name'];
-
-?>
-
-<h1>Ciao mi chiamo <?php echo $name ?> </h1>
 <form action="censored.php" method="GET">
     <input type="text" name="text" placeholder="Scrivi il tuo testo">
     <input type="text" name="word" placeholder="Che parola vuoi censurare?">
