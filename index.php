52a013c47fa01853db9d376728ebfb2d51d9070d
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-03-17 16:19:22 +0100

    FEAT add input and button

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 <?php
 
-$name = 'michele';
+$name = $_GET['name'];
 
 ?>
 
-<h1>Ciao mi chiamo <?php echo $name ?></h1>
+<h1>Ciao mi chiamo <?php echo $name ?> </h1>
+<form action="censored.php" method="GET">
+    <input type="text" name="text" placeholder="Scrivi il tuo testo">
+    <input type="text" name="word" placeholder="Che parola vuoi censurare?">
+    <button type="submit"> Invia</button>
+</form>
